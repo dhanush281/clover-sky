b9bf61dc450a74d21ac422321f1ae9641668a7f4
dhanush281 <dhanushvozh345@gmail.com>
2025-02-06 22:55:18 +0530

    sky: vendorsetup-vendor,kernel

---
+++ vendorsetup.sh
@@ -0,0 +1,7 @@
+# Clone the kernel source
+echo "Cloning kernel repository..."
+git clone https://github.com/dhanush281/android_xiaomi_sky_kernel.git device/xiaomi/sky-kernel
+
+# Clone the vendor source
+echo "Cloning vendor repository..."
+git clone https://github.com/dhanush281/android_vendor_xiaomi_sky.git vendor/xiaomi/sky
